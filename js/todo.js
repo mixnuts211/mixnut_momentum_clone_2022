a36e776e79a4c8d2d97787cb2fdc0af77ed17a82
mixnuts211 <migyeong0211@gmail.com>
2022-04-22 21:52:14 +0900

    4.22 save todo list

--- js/todo.js
+++ js/todo.js
@@ -3,13 +3,18 @@ const todoWrite = document.querySelector("input.todo-wrap-write-here");
 const addTodoBtn = document.querySelector(".todo-wrap-btn");
 const todoUl = document.querySelector(".todo-wrap-list");
 
+let toDos = [];
+function saveToDo(){
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function showTodoList(newTodoList){
     const li = document.createElement("li");
     const span = document.createElement("span");
     span.innerText = newTodoList;
     const input = document.createElement("input");
     input.setAttribute("type", "checkbox");
-    const deletBtn = document.createElement("button");
+    const deletBtn = document.createElement("div");
     deletBtn.innerText = "x";
     deletBtn.addEventListener("click", deletTodo);
     li.appendChild(input);
@@ -19,22 +24,30 @@ function showTodoList(newTodoList){
 }
 
 function deletTodo(event){
-    const deletLi = event.target.parentElement;
-    deletLi.remove();
+    const li = event.target.parentElement;
+    li.remove();
 }
 
-function todoBtnHandler(e){
-    e.preventDefault();
+function todoBtnHandler(event){
+    event.preventDefault();
     const newTodoList = todoWrite.value;
     todoWrite.value = "";
+    toDos.push(newTodoList);
     showTodoList(newTodoList);
+    saveToDo();
 }
 
 todoForm.addEventListener("submit",todoBtnHandler);
 
+const getSavedToDo = localStorage.getItem("todos");
+
+if (getSavedToDo !== null){
+    const parsedToDos = JSON.parse(getSavedToDo);
+    toDos = parsedToDos;
+    parsedToDos.forEach(showTodoList);
+}
+
+
 //-todo list-
-//enter가 원하지 않는 btn을 누르는것을 방지하기
-//li의 span과 delet btn의 간격 조정
-//edit와 delet를 더보기 메뉴로 묶기
-//localStorge에 save하기
+//localStorge에 저장해서 불러오기/삭제하기
 
