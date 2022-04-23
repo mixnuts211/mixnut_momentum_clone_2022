3c6ff75cf11d42f9e8e5bafb0dd2c0029c57352d
mixnuts211 <migyeong0211@gmail.com>
2022-04-23 22:25:31 +0900

    modify weather login css

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
 const weatherIcon = document.querySelector(".weather-icon");
-const weatherTemper = document.querySelector(".weather-temperature");
-const weatherLocation = document.querySelector(".weather-location");
+const weatherTemper = document.querySelector(".weather-geo-temperature");
+const weatherLocation = document.querySelector(".weather-geo-location");
 const weatherIconText = weatherIcon.querySelector("span");
 const API_KEY = "d2b326eaa8284481f83b8d344aea852d";
 
@@ -11,7 +11,7 @@ function onGeoTrue(position){
     const urlText = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
     fetch(urlText).then((response) => response.json()).then((date) => {
         weatherLocation.innerText = date.name;
-        weatherTemper.innerText = date.main.temp;
+        weatherTemper.innerText = `${date.main.temp}°`;
         const callIcon = date.weather[0].icon;
         const weatherIconImg = document.createElement("img");
         weatherIconImg.src = `http://openweathermap.org/img/wn/${callIcon}@2x.png`;
